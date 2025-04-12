74502f872a9b13969d1c147b266d98c4eabf0ab3
Abdulrahman1204 <aliabdulrahman1204@gmail.com>
2025-04-12 12:59:39 +0300

    fix 2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const ARTICLE_PER_PAGE = 6;
 
 export const request = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://ampere-five.vercel.app/api",
   withCredentials: true,
 });
